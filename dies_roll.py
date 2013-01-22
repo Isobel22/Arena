5633c6bbb1af62bea5e65565a1ce69d5e0eab417
Michaela Hrabakova <michaela.hrabakova@legendarne.cz>
2013-01-22 23:28:10 +0100

    formatting updated in dies_roll.py

--- dies_roll.py
+++ dies_roll.py
@@ -1,11 +1,13 @@
-from random import randint 
+from random import randint
+
 
 def die6():
-	return randint(1,6)
+    return randint(1, 6)
+
 
 def die10():
-	return randint(1,10)
-	
+    return randint(1, 10)
+
+
 def die20():
-	return randint(1,20)
-	
+    return randint(1, 20)
