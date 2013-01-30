2cac4dad4e272124b9b2c5e0b140f4dc63e5fbd9
Michaela Hrabakova <michaela.hrabakova@legendarne.cz>
2013-01-30 18:45:50 +0100

    combat_round() changed
    a realised, that not only I do'nt need last_styles as parameter of
    combat_round.
    I don't need this variable at all, current_style can work alone.

--- arena_game.py
+++ arena_game.py
@@ -82,13 +82,15 @@ class Arena(object):
     def combat(self, player_hp, opponent_hp):
         """ Basic scheme of combat, rounds counting, styles history, win/lose conditions, endings"""
         round = 1
-        self.player_last_style = standard
-        self.opponent_last_style = standard
+        #self.player_last_style = standard
+        #self.opponent_last_style = standard
+        self.player_current_style = standard
+        self.opponent_current_style = standard
         self.player_hp = player_hp
         self.opponent_hp = opponent_hp
         while self.player_hp > 0 and self.opponent_hp > 0:
             print "-------------------------------\nFighting round %s began!\n-------------------------------" % round
-            self.combat_round(self.player_last_style, self.opponent_last_style)
+            self.combat_round()
             round += 1
         if self.player_hp <= 0 and self.opponent_hp > 0:  # LOSE
             print "-------------------------------\n%s is falling to the ground, heavily wounded.\nYOU LOST THIS MATCH!!!\n------------------------------- " % self.player.name
@@ -98,63 +100,61 @@ class Arena(object):
             print "-------------------------------\nYou brutally hit %s in same moment, as %s hit you. You both are falling to the ground, heavily wounded.\nTHIS WAS A DOUBLE KILL!!!\n------------------------------- " % (self.opponent.name, self.opponent.name)
         raw_input("Press any key:")
 
-    def combat_round(self, player_current_style, opponent_current_style):
+    def combat_round(self):
         """Player turn"""
         print "You are attacking!!!\n-------------------------------"
 
-        if player_current_style == parry:  # if last action was parry or furious, this round has predefined after-style
-            player_current_style = after_parry
-        elif player_current_style == furious:
-            player_current_style = after_furious
+        if self.player_current_style == parry:  # if last action was parry or furious, this round has predefined after-style
+            self.player_current_style = after_parry
+        elif self.player_current_style == furious:
+            self.player_current_style = after_furious
         else:
-            player_current_style = self.player_choose_style()  # player chooses his style
-        print "You are using %s this round." % player_current_style.name
+            self.player_current_style = self.player_choose_style()  # player chooses his style
+        print "You are using %s this round." % self.player_current_style.name
 
-        player_attack_roll = player_current_style.attack_roll()  # attack roll
-        player_total_attack = player_attack_roll + self.player.attack + self.player.weapon.attack + player_current_style.attack_bonus  # players total attack
-        opponent_total_defense = self.opponent.defense + opponent_current_style.defense  # opponent total defense
-        print "You rolled %s, so %s's total attack this round is %s (%s+%s+%s+%s). \nYour opponent total defense is %s(%s+%s)." % (player_attack_roll, self.player.name, player_total_attack, player_attack_roll, self.player.attack, self.player.weapon.attack, player_current_style.attack_bonus, opponent_total_defense, self.opponent.defense, opponent_current_style.defense)
+        player_attack_roll = self.player_current_style.attack_roll()  # attack roll
+        player_total_attack = player_attack_roll + self.player.attack + self.player.weapon.attack + self.player_current_style.attack_bonus  # players total attack
+        opponent_total_defense = self.opponent.defense + self.opponent_current_style.defense  # opponent total defense
+        print "You rolled %s, so %s's total attack this round is %s (%s+%s+%s+%s). \nYour opponent total defense is %s(%s+%s)." % (player_attack_roll, self.player.name, player_total_attack, player_attack_roll, self.player.attack, self.player.weapon.attack, self.player_current_style.attack_bonus, opponent_total_defense, self.opponent.defense, self.opponent_current_style.defense)
 
         if player_total_attack > opponent_total_defense and player_attack_roll != 10:  # succesful hit
-            damage = self.player.weapon.damage_dealt() + player_current_style.damage_bonus
+            damage = self.player.weapon.damage_dealt() + self.player_current_style.damage_bonus
             self.opponent_hp -= damage
-            print "YOU SUCCESFULY HIT!\n%s %s and hit his foe, dealt %s damage (%s+%s). %s now have %s HPs left." % (self.player.name, player_current_style.attack_desc, damage, damage - player_current_style.damage_bonus, player_current_style.damage_bonus, self.opponent.name, self.opponent_hp)
+            print "YOU SUCCESFULY HIT!\n%s %s and hit his foe, dealt %s damage (%s+%s). %s now have %s HPs left." % (self.player.name, self.player_current_style.attack_desc, damage, damage - self.player_current_style.damage_bonus, self.player_current_style.damage_bonus, self.opponent.name, self.opponent_hp)
         elif player_attack_roll == 10:  # critical hit : automaticky hits opponent and deal +2 bonus damage
-            damage = self.player.weapon.damage_dealt() + 2 + player_current_style.damage_bonus
+            damage = self.player.weapon.damage_dealt() + 2 + self.player_current_style.damage_bonus
             self.opponent_hp -= damage
-            print "CRITICAL HIT!\n%s %s and fiercely hit his foe, dealt incredible %s damage(%s+%s+2 for critical). %s now have %s HPs left." % (self.player.name, player_current_style.attack_desc, damage, damage - 2 - player_current_style.damage_bonus, player_current_style.damage_bonus, self.opponent.name, self.opponent_hp)
+            print "CRITICAL HIT!\n%s %s and fiercely hit his foe, dealt incredible %s damage(%s+%s+2 for critical). %s now have %s HPs left." % (self.player.name, self.player_current_style.attack_desc, damage, damage - 2 - self.player_current_style.damage_bonus, self.player_current_style.damage_bonus, self.opponent.name, self.opponent_hp)
         else:  # missing opponent
-            print "YOU MISSED!\n%s %s, but missed his foe." % (self.player.name, player_current_style.attack_desc)
+            print "YOU MISSED!\n%s %s, but missed his foe." % (self.player.name, self.player_current_style.attack_desc)
         raw_input(">")
 
         """Opponent turn"""
         print "-------------------------------\nNow %s is attacking you!" % self.opponent.name
 
-        if opponent_current_style == parry:  # if last action was parry or furious, this round has predefined after-style
-            opponent_current_style = after_parry
-        elif opponent_current_style == furious:
-            opponent_current_style = after_furious
+        if self.opponent_current_style == parry:  # if last action was parry or furious, this round has predefined after-style
+            self.opponent_current_style = after_parry
+        elif self.opponent_current_style == furious:
+            self.opponent_current_style = after_furious
         else:
-            opponent_current_style = self.opponent_choose_style()  # opponent randomly chooses his style
-        print "%s is using %s this round." % (self.opponent.name, opponent_current_style.name)
+            self.opponent_current_style = self.opponent_choose_style()  # opponent randomly chooses his style
+        print "%s is using %s this round." % (self.opponent.name, self.opponent_current_style.name)
 
-        opponent_attack_roll = opponent_current_style.attack_roll()
-        opponent_total_attack = opponent_attack_roll + self.opponent.attack + self.opponent.weapon.attack + opponent_current_style.attack_bonus
-        player_total_defense = self.player.defense + player_current_style.defense
-        print "%s rolled %s, and has total attack for this round %s(%s+%s+%s+%s).\nYour defense is %s(%s+%s)" % (self.opponent.name, opponent_attack_roll, opponent_total_attack, opponent_attack_roll, self.opponent.attack, self.opponent.weapon.attack, opponent_current_style.attack_bonus, player_total_defense, self.player.defense, player_current_style.defense)
+        opponent_attack_roll = self.opponent_current_style.attack_roll()
+        opponent_total_attack = opponent_attack_roll + self.opponent.attack + self.opponent.weapon.attack + self.opponent_current_style.attack_bonus
+        player_total_defense = self.player.defense + self.player_current_style.defense
+        print "%s rolled %s, and has total attack for this round %s(%s+%s+%s+%s).\nYour defense is %s(%s+%s)" % (self.opponent.name, opponent_attack_roll, opponent_total_attack, opponent_attack_roll, self.opponent.attack, self.opponent.weapon.attack, self.opponent_current_style.attack_bonus, player_total_defense, self.player.defense, self.player_current_style.defense)
 
         if opponent_total_attack > player_total_defense and opponent_attack_roll != 10:  # succesful hit
-            damage = self.opponent.weapon.damage_dealt() + opponent_current_style.damage_bonus
+            damage = self.opponent.weapon.damage_dealt() + self.opponent_current_style.damage_bonus
             self.player_hp -= damage
-            print "YOU WERE WOUNDED!\n%s %s and hit you, dealt %s damage (%s+%s). %s now have %s HPs left." % (self.opponent.name, opponent_current_style.attack_desc, damage, damage - opponent_current_style.damage_bonus, opponent_current_style.damage_bonus, self.player.name, self.player_hp)
+            print "YOU WERE WOUNDED!\n%s %s and hit you, dealt %s damage (%s+%s). %s now have %s HPs left." % (self.opponent.name, self.opponent_current_style.attack_desc, damage, damage - self.opponent_current_style.damage_bonus, self.opponent_current_style.damage_bonus, self.player.name, self.player_hp)
         elif opponent_attack_roll == 10:  # critical hit
-            damage = self.opponent.weapon.damage_dealt() + opponent_current_style.damage_bonus + 2
+            damage = self.opponent.weapon.damage_dealt() + self.opponent_current_style.damage_bonus + 2
             self.player_hp -= damage
-            print "YOU SUFFERED CRITICAL HIT!\n%s %s and fiercely hit you, dealt incredible %s damage (%s+%s+2 for critical). %s now have %s HPs left." % (self.opponent.name, opponent_current_style.attack_desc, damage, damage - opponent_current_style.damage_bonus - 2, opponent_current_style.damage_bonus, self.player.name, self.player_hp)
+            print "YOU SUFFERED CRITICAL HIT!\n%s %s and fiercely hit you, dealt incredible %s damage (%s+%s+2 for critical). %s now have %s HPs left." % (self.opponent.name, self.opponent_current_style.attack_desc, damage, damage - self.opponent_current_style.damage_bonus - 2, self.opponent_current_style.damage_bonus, self.player.name, self.player_hp)
         else:
-            print "YOU DEFENDED YOURSELF!\n%s %s, but missed you!" % (self.opponent.name, opponent_current_style.attack_desc)
-        self.player_last_style = player_current_style
-        self.opponent_last_style = opponent_current_style
+            print "YOU DEFENDED YOURSELF!\n%s %s, but missed you!" % (self.opponent.name, self.opponent_current_style.attack_desc)
         raw_input(">")
 
     def player_choose_style(self):
